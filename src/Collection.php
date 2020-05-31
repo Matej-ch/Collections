3ef103b2d99bbd587fa4de94fa6cf0ad6a4bb78d
Matej <matejchalachan@gmail.com>
2020-05-31 12:46:01 +0200

    First pass on collections

--- src/Collection.php
+++ src/Collection.php
@@ -4,11 +4,13 @@
 namespace App;
 
 
-class Collection
+use Exception;
+use Traversable;
+
+class Collection implements \ArrayAccess, \Countable, \IteratorAggregate
 {
     private $items;
-
-
+    
     public function __construct($items)
     {
         $this->items = $items;
@@ -24,13 +26,97 @@ class Collection
         return new static(array_map($callback,$this->items));
     }
 
+    public function each($callback)
+    {
+
+    }
+
+    public function pluck($key)
+    {
+
+    }
+
+    public function flatten($callback)
+    {
+
+    }
+
+    public function last()
+    {
+
+    }
+
     public function filter($callback)
     {
         return new static(array_filter($this->items,$callback));
     }
 
+    public function reject($callback)
+    {
+        return new static($this->filter($callback));
+    }
+
+    public function reduce($callback,$init)
+    {
+        return new static(array_reduce($this->items,$callback,$init));
+    }
+
+    public function sum()
+    {
+        return new static(array_sum($this->items));
+    }
+
+    public function reverse()
+    {
+        return new static(array_reverse($this->items));
+    }
+
+    public function values()
+    {
+        return new static(array_values($this->items));
+    }
+
+    public function keys()
+    {
+        return new static(array_keys($this->items));
+    }
+
     public function toArray()
     {
         return $this->items;
+    }
+
+    public function offsetExists($offset)
+    {
+        return array_key_exists($offset,$this->items);
+    }
+
+    public function offsetGet($offset)
+    {
+       return $this->items[$offset];
+    }
+
+    public function offsetSet($offset, $value)
+    {
+        if($offset === null) {
+            $this->items[] = $value;
+        } else {
+            $this->items[$offset] = $value;
+        }
+    }
+
+    public function offsetUnset($offset)
+    {
+         unset($this->items[$offset]);
+    }
+
+    public function count()
+    {
+        return count($this->items);
+    }
+
+    public function getIterator()
+    {
+        return new \ArrayIterator($this->items);
     }
 }
--- tests/CollectionTest.php
+++ tests/CollectionTest.php
@@ -1,9 +1,90 @@
 <?php
 
 
+use App\Collection;
 use PHPUnit\Framework\TestCase;
 
 class CollectionTest extends TestCase
 {
 
+    /** @test */
+    function it_returns_instance_of_collection() {
+        $collection = Collection::make($this->getTestItems());
+
+        $this->assertInstanceOf(Collection::class,$collection);
+    }
+
+    /** @test */
+    function it_returns_item_array() {
+        $collection = Collection::make($this->getTestItems());
+
+        $this->assertIsArray($collection->toArray());
+    }
+
+    /** @test */
+    function it_returns_size_of_collection() {
+        $collection = Collection::make($this->getTestItems());
+
+        $this->assertCount(6,$collection);
+    }
+
+    /** @test */
+    function it_returns_size_of_collection_with_function() {
+        $collection = Collection::make($this->getTestItems());
+
+        $this->assertEquals(6,$collection->count());
+    }
+
+    function getTestItems() {
+        return [
+            (object)[
+                'value' => 2,
+                'email' => 'abc@test.com',
+                'info' => [
+                    'price' => 12.5,
+                    'pieces' => 2
+                ]
+            ],
+            (object)[
+                'value' => 5,
+                'email' => 'zcxx@test.com',
+                'info' => [
+                    'price' => 12.98,
+                    'pieces' => 9
+                ]
+            ],
+            (object)[
+                'value' => 420,
+                'email' => 'tendies@local.com',
+                'info' => [
+                    'price' => 65,
+                    'pieces' => 66
+                ]
+            ],
+            (object)[
+                'value' => 69,
+                'email' => 'zumba@test.com',
+                'info' => [
+                    'price' => 12.5,
+                    'pieces' => 3
+                ]
+            ],
+            (object)[
+                'value' => 3,
+                'email' => 'quak5@test.com',
+                'info' => [
+                    'price' => 15,
+                    'pieces' => 1
+                ]
+            ],
+            (object)[
+                'value' => 8,
+                'email' => 'as5s52@test.com',
+                'info' => [
+                    'price' => 0.256,
+                    'pieces' => 5
+                ]
+            ],
+        ];
+    }
 }
