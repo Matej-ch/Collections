e17b00e2f704cefd2a22785bf2db934c964867ef
Matej <matejchalachan@gmail.com>
2020-06-11 20:17:53 +0200

    search method and Tests

---
+++ tests/CollectionSearchTests.php
@@ -0,0 +1,35 @@
+<?php
+
+
+use PHPUnit\Framework\TestCase;
+
+class CollectionSearchTests extends TestCase
+{
+    /** @test */
+    function it_returns_false_if_value_is_not_in_collection() {
+        $collection = \App\Collection::make(['Alex', 'John', 'Jason', 'Martyn', 'Hanlin']);
+
+        $this->assertFalse($collection->search('Matt'));
+    }
+
+    /** @test */
+    function it_returns_key_if_value_is_in_collection() {
+        $collection = \App\Collection::make(['Alex', 'John', 'Jason', 'Martyn', 'Hanlin']);
+
+        $this->assertEquals(2,$collection->search('Jason'));
+    }
+
+    /** @test */
+    function it_returns_key_if_value_is_in_collection_with_strict_comparison() {
+        $collection = \App\Collection::make([1,2,3,45,6,78,9]);
+
+        $this->assertSame(1,$collection->search(2));
+    }
+
+    /** @test */
+    function it_returns_false_if_value_not_in_collection_with_strict_comparison() {
+        $collection = \App\Collection::make([1,2,3,45,6,78,9]);
+
+        $this->assertFalse($collection->search('3'));
+    }
+}
