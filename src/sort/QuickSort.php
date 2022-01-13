27128f935c2417efd5c60e7e387d06b818bb9d98
Matej <matejchalachan@gmail.com>
2022-01-13 19:36:37 +0100

    quick sort added (broken still)

--- src/sort/QuickSort.php
+++ src/sort/QuickSort.php
@@ -4,8 +4,34 @@ namespace App\sort;
 
 class QuickSort
 {
-    public static function sort(array $data,$direction = SORT_ASC): array
+    public static function sort(array $data, $direction = SORT_ASC): array
     {
-        return [];
+        return self::quickSort($data,0,count($data) - 1);
+    }
+
+    private static function quickSort(array $data, int $left, int $right): array
+    {
+        if ($left < $right) {
+            $boundary = $left;
+            for ($i = $left + 1; $i < $right; $i++) {
+                if ($data[$i] > $data[$left]) {
+                    self::swap($data, $i, ++$boundary);
+                }
+            }
+            $data = self::swap($data, $left, $boundary);
+            self::quickSort($data, $left, $boundary);
+            self::quickSort($data, $boundary + 1, $right);
+        }
+
+        return $data;
+    }
+
+    private static function swap(array $data, int $left, int $right): array
+    {
+        $tmp = $data[$right];
+        $data[$right] = $data[$left];
+        $data[$left] = $tmp;
+
+        return $data;
     }
 }
