5124bcb61cd7ed4a2149e0e93bb56552ef0ca183
Matej <matejchalachan@gmail.com>
2022-01-14 22:36:54 +0100

    radix sort (numbers only)

--- src/sort/RadixSort.php
+++ src/sort/RadixSort.php
@@ -4,8 +4,65 @@ namespace App\sort;
 
 class RadixSort implements ISort
 {
-    public static function sort(array $data,int $direction = SORT_ASC): array
+    public static function sort(array $data, int $direction = SORT_ASC): array
     {
+        // Array for 10 queues.
+        $queues = [
+            [], [], [], [], [], [], [], [], [], []
+        ];
 
+        // Queues are allocated dynamically. In first iteration longest digits
+        // element also determined.
+        $longest = 0;
+        foreach ($data as $el) {
+
+            if(is_numeric($el)) {
+                $condition = $el > $longest;
+            } else {
+                $condition = ord($el) > $longest;
+            }
+
+            if ($condition) {
+                $longest = $el;
+            }
+
+            if(is_numeric($el)) {
+                $queues[$el % 10][] = $el;
+            } else {
+                $queues[ord($el) % 10][] = $el;
+            }
+        }
+
+        // Queues are dequeued back into original elements.
+        $i = 0;
+        foreach ($queues as $key => $q) {
+            while (!empty($queues[$key])) {
+                $data[$i++] = array_shift($queues[$key]);
+            }
+        }
+
+        // Remaining iterations are determined based on longest digits element.
+        $it = strlen($longest) - 1;
+        $d = 10;
+        while ($it--) {
+            foreach ($data as $el) {
+                if(is_numeric($el)) {
+                    $queues[floor($el / $d) % 10][] = $el;
+                } else {
+                    $queues[floor(ord($el) / $d) % 10][] = $el;
+                }
+
+            }
+            $i = 0;
+            foreach ($queues as $key => $q) {
+                while (!empty($queues[$key])) {
+                    $data[$i++] = array_shift($queues[$key]);
+                }
+            }
+            $d *= 10;
+        }
+
+        return $data;
     }
+
 }
--- tests/RadixSortTest.php
+++ tests/RadixSortTest.php
@@ -2,6 +2,7 @@
 
 namespace tests;
 
+use App\sort\RadixSort;
 use PHPUnit\Framework\TestCase;
 
 class RadixSortTest extends TestCase
@@ -13,8 +14,9 @@ class RadixSortTest extends TestCase
      * @param $sorted
      * @param $unsorted
      */
-    function it_sorts_array_of_values_from_lowest($sorted,$unsorted) {
-
+    function it_sorts_array_of_values_from_lowest($sorted,$unsorted)
+    {
+        $this->assertEquals($sorted,RadixSort::sort($unsorted));
     }
 
     /**
@@ -23,7 +25,8 @@ class RadixSortTest extends TestCase
      * @param $sorted
      * @param $unsorted
      */
-    function it_sorts_array_of_values_from_highest($sorted,$unsorted) {
-
+    function it_sorts_array_of_values_from_highest($sorted,$unsorted)
+    {
+        $this->assertEquals($sorted,RadixSort::sort($unsorted,SORT_DESC));
     }
 }
