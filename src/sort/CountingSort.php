de35a1f07a567e1baad1b1798ab9e5bcdcf833f5
Matej <matejchalachan@gmail.com>
2022-05-15 01:22:32 +0200

    counting sort simple way

--- src/sort/CountingSort.php
+++ src/sort/CountingSort.php
@@ -4,8 +4,47 @@ namespace App\sort;
 
 class CountingSort implements ISort
 {
-    public static function sort(array $data,int $direction = SORT_ASC): array
+    public static function sort(array $data, int $direction = SORT_ASC): array
     {
+        $aux = [];
+        if(empty($data)) {
+            return $aux;
+        }
 
+        $min = $data[0];
+        $max = $data[0];
+
+        for ($i = 1; $i < count($data); $i++) {
+            if ($data[$i] < $min) {
+                $min = $data[$i];
+            } elseif ($data[$i] > $max) {
+                $max = $data[$i];
+            }
+        }
+
+        $counts = [];
+        for ($i = 0; $i < $max - $min + 1;$i++) {
+            $counts[] = 0;
+        }
+
+        foreach ($data as $iValue) {
+            $counts[$iValue - $min]++;
+        }
+
+        $counts[0]--;
+        for ($i = 1; $i < count($counts); $i++) {
+            $counts[$i] += $counts[$i - 1];
+        }
+
+
+        for ($i = count($data) - 1; $i >= 0; $i--) {
+            $aux[$counts[$data[$i] - $min]--] = $data[$i];
+        }
+
+        if($direction === SORT_DESC) {
+            rsort($aux);
+        }
+
+        return $aux;
     }
 }
--- tests/CountingSortTest.php
+++ tests/CountingSortTest.php
@@ -2,6 +2,7 @@
 
 namespace tests;
 
+use App\sort\CountingSort;
 use PHPUnit\Framework\TestCase;
 
 class CountingSortTest extends TestCase
@@ -9,21 +10,23 @@ class CountingSortTest extends TestCase
     use SortDataProvider;
     /**
      * @test
-     * @dataProvider arraysFromLowest
+     * @dataProvider arraysIntegerFromLowest
      * @param $sorted
      * @param $unsorted
      */
-    function it_sorts_array_of_values_from_lowest($sorted,$unsorted) {
-
+    function it_sorts_array_of_values_from_lowest($sorted,$unsorted)
+    {
+        $this->assertEquals($sorted,CountingSort::sort($unsorted));
     }
 
     /**
      * @test
-     * @dataProvider arraysFromHighest
+     * @dataProvider arraysIntegerFromHighest
      * @param $sorted
      * @param $unsorted
      */
-    function it_sorts_array_of_values_from_highest($sorted,$unsorted) {
-
+    function it_sorts_array_of_values_from_highest($sorted,$unsorted)
+    {
+        $this->assertEquals($sorted,CountingSort::sort($unsorted,SORT_DESC));
     }
 }
